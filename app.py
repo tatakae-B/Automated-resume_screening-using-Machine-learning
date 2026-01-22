7ec11642628a3d7068aec28e0b1a05a90763eb1f
Badarinadh A P <badarinadh4580@gmail.com>
2026-01-22 19:34:37 +0530

    Trigger fresh Vercel deployment

--- app.py
+++ app.py
@@ -57,3 +57,5 @@ def index():
 if __name__ == '__main__':
     debug = os.environ.get('FLASK_ENV') == 'development'
     app.run(debug=debug, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+# Cache buster: 01/22/2026 19:34:37
