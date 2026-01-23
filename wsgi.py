24c1c27ea7d6d155b0e15e4af2a1bd600975a0b0
Badarinadh A P <badarinadh4580@gmail.com>
2026-01-23 09:07:40 +0530

    Fix: Properly export app from wsgi.py + add script entry in pyproject.toml

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
 """WSGI entrypoint for Vercel"""
+import sys
+import os
+
+# Add api directory to path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from api.index import app
 
+# Export app for Vercel
 if __name__ == '__main__':
     app.run()
+
